ff211f54fb3cc71556c7e8854df40dd3829f87de
Mariano Casarino <mariano.e.casarino@gmail.com>
2026-04-07 01:21:01 -0300

    Implementacion del flujo OTP y logica de recuperacion de cuenta

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from encrypted_model_fields.fields import EncryptedCharField
+from django.utils import timezone
+import random
 
 # Create your models here.
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     encrypted_info = EncryptedCharField(max_length=100)  # Example encrypted field
+
+class OTPCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+    is_used = models.BooleanField(default=False)
+
+    def is_valid(self):
+        return not self.is_used and timezone.now() < self.expires_at
+    
+    def __srt__(self):
+        return f"OTP para {self.user.username} - {self.code}"
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -30,3 +30,92 @@ class LoginView(APIView):
                 'user': UserSerializer(user).data
             })
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+from django.utils import timezone
+from datetime import timedelta
+import random
+from .models import OTPCode
+
+class ForgotPasswordView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        email = request.data.get('email')
+        try:
+            user = User.objets.get(email=email)
+        except User.DoesNotExist:
+            return Response ({'message': 'Si el email existe recibiras el codigo.'})
+        
+        OTPCode.objects.filter(user=user, is_used=False).update(is_used=True)
+
+        code = str(random.randint(100000, 999999))
+        expires_at = timezone.now() + timedelta(minutes=10)
+
+        OTPCode.objects.create(user=user, code=code, expires_at=expires_at)
+
+        print(f"\n{'='*40}")
+        print(f" OTP para {user.username} ({user.email}): {code}")
+        print(f" Válido por 10 minutos")
+        print(f"{'='*40}\n")
+
+        return Response({'message:' 'Si el email existe, revibirás el código.'})
+    
+class VerifyOTPView(APIView):
+    permission_classes = (AllowAny,)
+    
+    def post(self, request):
+        email = request.data.get('email')
+        code = request.data.get('code')
+
+        try:
+            user = User.objects.get(email=email)
+            otp = OTPCode.objects.filter(
+                user=user, code=code, is_used=False
+            ).latest('created_at')
+        except (User.DoesNotExist, OTPCode.DoesNotExist):
+            return Response(
+                {'error': 'Código inválido.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        if not otp.is_valid():
+            return Response(
+                {'error': 'El código expiró'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        return Response({'message': 'Código válido.', 'email':email})
+    
+
+class ResetPasswordView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        email = request.data.get('email')
+        code = request.data.get('code')
+        new_password = request.get.data('new_password')
+
+        try:
+            user = User.objects.get(email=email)
+            otp = OTPCode.objects.filter(
+                user=user, code=code, is_used=False
+            ).latest('created_at')
+        except (User.DoesNotExist, OTPCode.DoesNotExist):
+            return Response(
+                {'error': 'Código Inválido.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        if not otp.is_valid():
+            return Response(
+                {'error': 'El código expiró.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        user.set_password(new_password)
+        user.save()
+        otp.is_used = True
+        otp.save()
+
+        return Response ({'message': 'Contraseña actualizada correctamente'})
+        
